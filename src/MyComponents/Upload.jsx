14b6f0291d910d83214a76b64039c1045d3804a0
Utkarsh Dixit <utkarshdixit2021@gmail.com>
2022-07-16 18:43:28 +0530

    Update Upload.jsx

--- src/MyComponents/Upload.jsx
+++ src/MyComponents/Upload.jsx
@@ -1,26 +1,17 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 const Upload = () => {
   return (
     <div style={{ backgroundColor: "#Dbe6ea" }}>
-      <Link to={"/notes"}>
-        <button className="btn btn-info my-4">
-          &lt;-- Go Back to Notes Page
-        </button>
-      </Link>
       <h1>Upload Your Own Notes and Papers here!!</h1>
+      <h2>Click the button below</h2>
       <div className="upload-btn-wrapper">
-        <a href="https://forms.gle/" target="_blank" rel="noopener noreferrer">
-        <button className="btn btn-outline-dark mx-2 my-2">
-          Upload Your Notes Here
+        <a href="https://forms.gle/7K3mvafsxDsecFkVA" target="_blank" rel="noopener noreferrer">
+        <button className="btn btn-dark mx-2 my-2">
+          Upload Your Notes and Papers Here
         </button>
         </a>
-        <a href="https://forms.gle/" target="_blank" rel="noopener noreferrer">
-        <button className="btn btn-outline-dark mx-2 my-2">
-          Upload Your Papers Here
-        </button>
-        </a>
+        <p className="my-0">Note: You'll be redirected to a google form and there you can upload your Notes.</p>
       </div>
     </div>
   );

37a57f87dedd41700e12d5cea23c554c652ef13a
Utkarsh Dixit <utkarshdixit2021@gmail.com>
2022-07-22 12:14:57 +0530

    Update ContactUs.jsx

--- src/MyComponents/ContactUs.jsx
+++ src/MyComponents/ContactUs.jsx
@@ -38,152 +38,158 @@ const ContactUs = () => {
   }, 5000);
 
   return (
-    <form action="" onSubmit={sendEmail} style={{ backgroundColor: "#Dbe6ea" }}>
+    <>
       <Helmet>
         <title>Contact Us</title>
         <meta name="description" content="Contact us by filling the form" />
       </Helmet>
-      <section class="text-gray-600 body-font relative">
-        <div class="container px-5 py-8 mx-auto">
-          <div class="flex flex-col text-center w-full mb-3">
-            <h1 class="sm:text-3xl text-2xl font-medium title-font mb-0 mt-0 text-gray-900">
-              Contact Us
-            </h1>
-            <p
-              class="lg:w-2/3 mx-auto leading-relaxed text-base fw-bold "
-              style={{ marginBottom: "5px" }}
-            >
-              Message Any Doubt Below{" "}
-              <i class="bi bi-arrow-down-square-fill ms-1"></i>
-            </p>
-          </div>
-          <div class="lg:w-1/2 md:w-2/3 mx-auto">
-            <div class="flex flex-wrap -m-2">
-              <div class="p-2 w-1/2">
-                <div class="relative">
-                  <label
-                    for="name"
-                    class="leading-7 text-sm text-gray-600 fw-bold"
-                  >
-                    Name
-                  </label>
-                  <input
-                    type="text"
-                    id="name"
-                    name="name"
-                    class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                  />
+      <form
+        action=""
+        onSubmit={sendEmail}
+        style={{ backgroundColor: "#Dbe6ea" }}
+      >
+        <section class="text-gray-600 body-font relative">
+          <div class="container px-5 py-8 mx-auto">
+            <div class="flex flex-col text-center w-full mb-3">
+              <h1 class="sm:text-3xl text-2xl font-medium title-font mb-0 mt-0 text-gray-900">
+                Contact Us
+              </h1>
+              <p
+                class="lg:w-2/3 mx-auto leading-relaxed text-base fw-bold "
+                style={{ marginBottom: "5px" }}
+              >
+                Message Any Doubt Below{" "}
+                <i class="bi bi-arrow-down-square-fill ms-1"></i>
+              </p>
+            </div>
+            <div class="lg:w-1/2 md:w-2/3 mx-auto">
+              <div class="flex flex-wrap -m-2">
+                <div class="p-2 w-1/2">
+                  <div class="relative">
+                    <label
+                      for="name"
+                      class="leading-7 text-sm text-gray-600 fw-bold"
+                    >
+                      Name
+                    </label>
+                    <input
+                      type="text"
+                      id="name"
+                      name="name"
+                      class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                    />
+                  </div>
                 </div>
-              </div>
-              <div class="p-2 w-1/2">
-                <div class="relative">
-                  <label
-                    for="email"
-                    class="leading-7 text-sm text-gray-600 fw-bold"
-                  >
-                    Email
-                  </label>
-                  <input
-                    type="email"
-                    id="email"
-                    name="email"
-                    class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                  />
+                <div class="p-2 w-1/2">
+                  <div class="relative">
+                    <label
+                      for="email"
+                      class="leading-7 text-sm text-gray-600 fw-bold"
+                    >
+                      Email
+                    </label>
+                    <input
+                      type="email"
+                      id="email"
+                      name="email"
+                      class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                    />
+                  </div>
                 </div>
-              </div>
-              <div class="p-2 w-full">
-                <div class="relative">
-                  <label
-                    for="message"
-                    class="leading-7 text-sm text-gray-600 fw-bold"
-                  >
-                    Message
-                  </label>
-                  <textarea
-                    id="message"
-                    name="message"
-                    class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
-                  ></textarea>
+                <div class="p-2 w-full">
+                  <div class="relative">
+                    <label
+                      for="message"
+                      class="leading-7 text-sm text-gray-600 fw-bold"
+                    >
+                      Message
+                    </label>
+                    <textarea
+                      id="message"
+                      name="message"
+                      class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+                    ></textarea>
+                  </div>
                 </div>
-              </div>
-              <div class="p-2 w-full">
-                <button class="flex mx-auto text-white bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-blue-600 rounded text-lg">
-                  Contact Us
-                </button>
-              </div>
-              <div className="row">{result ? <Result /> : null}</div>
+                <div class="p-2 w-full">
+                  <button class="flex mx-auto text-white bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-blue-600 rounded text-lg">
+                    Contact Us
+                  </button>
+                </div>
+                <div className="row">{result ? <Result /> : null}</div>
 
-              <div class="p-2 w-full pt-8 mt-8 border-t border-gray-200 text-center">
-                <a
-                  class="text-blue-500"
-                  href="mailto:contact@bunk4study.com"
-                  target="_blank"
-                  text-decoration="none"
-                  rel="noreferrer"
-                >
-                  contact@bunk4study.com
-                </a>
-                <br />
-                <br />
-                <span
-                  class="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start"
-                  style={{ fontSize: "20px" }}
-                >
+                <div class="p-2 w-full pt-8 mt-8 border-t border-gray-200 text-center">
                   <a
-                    class="text-gray-500"
-                    href="https://www.facebook.com/Bunk4Study-100731219267632"
+                    class="text-blue-500"
+                    href="mailto:contact@bunk4study.com"
                     target="_blank"
+                    text-decoration="none"
                     rel="noreferrer"
                   >
-                    <i class="bi bi-facebook"></i>
+                    contact@bunk4study.com
                   </a>
-                  <a
-                    class="ml-3 text-gray-500"
-                    href="https://twitter.com/Bunk4Study?t=9MGpXgxPPs7TvQ0xWATLhQ&s=09"
-                    target="_blank"
-                    rel="noreferrer"
+                  <br />
+                  <br />
+                  <span
+                    class="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start"
+                    style={{ fontSize: "20px" }}
                   >
-                    <i class="bi bi-twitter"></i>
-                  </a>
-                  <a
-                    class="ml-3 text-gray-500"
-                    href="https://instagram.com/bunk4study?utm_medium=copy_link"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <i class="bi bi-instagram"></i>
-                  </a>
-                  <a
-                    class="ml-3 text-gray-500 "
-                    href="https://www.linkedin.com/company/bunk4study/?viewAsMember=true"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <i class="bi bi-linkedin"></i>
-                  </a>
-                  <a
-                    class="ml-3 text-gray-500"
-                    href="https://telegram.me/bunk4study"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <i class="bi bi-telegram"></i>
-                  </a>
-                  <a
-                    class="ml-3 text-gray-500"
-                    href="https://discord.gg/jCpBSH8mHF"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <i class="bi bi-discord"></i>
-                  </a>
-                </span>
+                    <a
+                      class="text-gray-500"
+                      href="https://www.facebook.com/Bunk4Study-100731219267632"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <i class="bi bi-facebook"></i>
+                    </a>
+                    <a
+                      class="ml-3 text-gray-500"
+                      href="https://twitter.com/Bunk4Study?t=9MGpXgxPPs7TvQ0xWATLhQ&s=09"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <i class="bi bi-twitter"></i>
+                    </a>
+                    <a
+                      class="ml-3 text-gray-500"
+                      href="https://instagram.com/bunk4study?utm_medium=copy_link"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <i class="bi bi-instagram"></i>
+                    </a>
+                    <a
+                      class="ml-3 text-gray-500 "
+                      href="https://www.linkedin.com/company/bunk4study/?viewAsMember=true"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <i class="bi bi-linkedin"></i>
+                    </a>
+                    <a
+                      class="ml-3 text-gray-500"
+                      href="https://telegram.me/bunk4study"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <i class="bi bi-telegram"></i>
+                    </a>
+                    <a
+                      class="ml-3 text-gray-500"
+                      href="https://discord.gg/jCpBSH8mHF"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <i class="bi bi-discord"></i>
+                    </a>
+                  </span>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </section>
-    </form>
+        </section>
+      </form>
+    </>
   );
 };
 
